3ce20c594c72ad31e901c2bac56657509a511147
Adriano Neres <ah.driano@gmail.com>
2020-04-27 23:04:37 -0400

    chore: manage transactions

---
+++ README.md
@@ -0,0 +1,3 @@
+# Desafio Database upload - Bootcamp GoStack 11
+
+Solução do desafio do módulo **Database upload**, da [@rocketseat](https://github.com/rocketseat), conforme [especificação](https://github.com/Rocketseat/bootcamp-gostack-desafios/tree/master/desafio-database-upload).
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,54 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository, getRepository } from 'typeorm';
 
+import AppError from '../errors/AppError';
+import TransactionRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+
+interface Request {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionRepository = getCustomRepository(TransactionRepository);
+    const categoryRepository = getRepository(Category);
+
+    const { total } = await transactionRepository.getBalance();
+
+    if (type === 'outcome' && total < value) {
+      throw new AppError('Insufficient funds.');
+    }
+
+    let relatedCategory = await categoryRepository.findOne({
+      where: {
+        title: category,
+      },
+    });
+
+    if (!relatedCategory) {
+      relatedCategory = categoryRepository.create({ title: category });
+      await categoryRepository.save(relatedCategory);
+    }
+
+    const transaction = transactionRepository.create({
+      title,
+      value,
+      type,
+      category: relatedCategory,
+    });
+
+    await transactionRepository.save(transaction);
+
+    return transaction;
   }
 }
 
